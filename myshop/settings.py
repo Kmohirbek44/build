d328fc3d9b91e84bae1ec6b9c1de37929fb082e2
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-08-27 20:46:15 +0500

    commit2

--- myshop/settings.py
+++ myshop/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATICFILES_DIRS=os.path.join(BASE_DIR , 'staticfiles')
+STATICFILES_DIRS=(os.path.join(BASE_DIR , 'staticfiles'),)
 STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
