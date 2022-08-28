0706a11bb6f93450055351e381639962a88ee835
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-08-28 14:12:19 +0500

    edit gitignore111

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -150,7 +150,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'static'),
 )
-STATIC_ROOT=os.path.join(BASE_DIR,'/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
