526782af9cd4b09fea99604570b14e3d8097259f
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-08-28 10:59:30 +0500

    c2

--- myshop/settings.py
+++ myshop/settings.py
@@ -145,10 +145,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'staticfiles'),
+)
+STATIC_ROOT=os.path.join(BASE_DIR,'/staticfiles/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR , 'staticfiles'),)
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
