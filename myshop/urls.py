651ebf6d2242873a30c777a33c4e652165f078aa
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-08-29 15:48:23 +0500

    finish1

--- myshop/urls.py
+++ myshop/urls.py
@@ -26,8 +26,5 @@ urlpatterns = [
     path('charst/', include('charts.urls')),
     path('contact/', include('contact.urls')),
 
-]
+]++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
